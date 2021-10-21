a18db2ba6e51bed34070f3d2db50ef65b4cdda65
Ivo <ivo.diogo@gmail.com>
2021-10-21 18:08:49 +0100

    isalpha done

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,26 +6,26 @@
 /*   By: ivda-cru <ivda-cru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/20 18:55:04 by ivda-cru          #+#    #+#             */
-/*   Updated: 2021/10/20 20:40:44 by ivda-cru         ###   ########.fr       */
+/*   Updated: 2021/10/21 16:32:03 by ivda-cru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <ctype.h>
+#include <stdlib.h>
 
-int ft_isalpha(int c)
+int	ft_isalpha(int c)
 {
-    if(c > 0 && c < 9)
-    return (0);
-
-    return (1);
-
-
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1);
+	else
+		return (0);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-
-    printf("%d\n", ft_isalpha(argv[]));
+	/* int a = atoi(argv[1]);
+	printf("%d\n", a); */
+	printf("%d\n", ft_isalpha('A'));
     return(0);
 }
