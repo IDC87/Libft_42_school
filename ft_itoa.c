091f2aa8f2edc028412b812e886d9e195f466b26
Ivo Cruz <ivo.diogo@gmail.com>
2021-11-24 16:26:34 +0000

    Wed Nov 24 16:26:34 WET 2021

--- ft_itoa.c
+++ ft_itoa.c
@@ -28,49 +28,45 @@ int itoa_size(int n)
 char *ft_itoa(int n)
 {       
     int i;
-    int a;
+    int z;
+    int len;
     char *str;
 
-    a = itoa_size(n);    
+    len = itoa_size(n);    
     i = 0;   
-    str = (char *)malloc(sizeof(char) * (a + 1));
+    z = 0;
+    str = (char *)malloc(sizeof(char) * (len + 1));
      if(str == NULL)
-    return (NULL);
-
-    if (n < 0)
-    {
-        n = n * (-1); // FALTA CALCULAR DE MANEIRA A METER O SINAL EM STRING NO FINAL
-    }
+        return (NULL);    
 
     if (n == 0)
-    *(str + i) = '0'; 
+        str[0] = '0'; 
 
-    while(a--)
+    if (n == -2147483648)
+        return(strdup("-2147483648"));
+
+    while(len--)
     {
-        printf("\n%d\n", n);
-        str[a] = (n % 10) + '0';
-        n = n / 10;       
-        i++;
-    }
+        if (n < 0)
+        {
+            n = n * (-1); //transforma o numero em positivo               
+            len = len + 1;
+            z = 1;  // numero de controlo para saber a frente que vem de um negativo             
+        }                      
+        str[len] = (n % 10) + '0';        
+        n = n / 10;
 
-     
-
-    //printf("\n%d \n\n", n);    
-
-    //printf("\n%d \n\n", i);
-
-
-    return (str);
-
+        if (z == 1)    
+            str[0]= '-'; 
     
-
-   
-
+        i++;
+    }   
+    return (str);
 }
 
-int main(int argc, char **argv)
+/* int main(int argc, char **argv)
 {
-    printf("\n\n %s \n", ft_itoa(-23232));
+    printf("\n\n %s \n", ft_itoa(-2147483648));
 
     return 0;
-}
+} */
