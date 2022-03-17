b9f83d7a35a2d0b91958829d4ed94ada5c6dcb7e
Ivo <ivo.diogo@gmail.com>
2022-03-17 20:03:26 +0000

    functions more optimized

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: ivda-cru <ivda-cru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 23:25:53 by ivda-cru          #+#    #+#             */
-/*   Updated: 2021/12/08 17:05:26 by ivda-cru         ###   ########.fr       */
+/*   Updated: 2022/03/17 19:41:31 by ivda-cru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,19 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	i;
-	size_t	j;
-	char	*str;
+	int		i;
+	char	*ret;
+	size_t	len;
 
-	if (!(s1) || !(s2))
+	len = ft_strlen(s1) + ft_strlen(s2);
+	ret = malloc(sizeof(char) * (len + 1));
+	if (!ret)
 		return (0);
 	i = 0;
-	j = 0;
-	str = (char *)malloc(sizeof(*s1) * (ft_strlen(s1) + ft_strlen(s2)));
-	if (str == NULL)
-		return (NULL);
-	while (i < ft_strlen(s1))
-	{
-		str[i] = s1[i];
-		i++;
-	}
-	while (j < ft_strlen(s2))
-	{
-		str[i + j] = s2[j];
-		j++;
-	}
-	str[i + j] = '\0';
-	return (str);
+	while (*s1)
+		ret[i++] = *s1++;
+	while (*s2)
+		ret[i++] = *s2++;
+	ret[i] = 0;
+	return (ret);
 }
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: ivda-cru <ivda-cru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 21:14:46 by ivda-cru          #+#    #+#             */
-/*   Updated: 2021/12/10 20:55:15 by ivda-cru         ###   ########.fr       */
+/*   Updated: 2022/03/17 19:41:53 by ivda-cru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,17 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
+	char	*ret;
+	char	*tmp;
 	size_t	i;
-	char	*str;
 
-	if (!s)
-		return (NULL);
+	ret = malloc(sizeof(char) * (len + 1));
+	if (!ret)
+		return (0);
+	tmp = (char *)s + start;
 	i = 0;
-	if (len > ft_strlen(s))
-		len = ft_strlen(s);
-	str = (char *)malloc(sizeof(char) * (len + 1));
-	if (str == NULL)
-		return (NULL);
-	while (i < len && start < ft_strlen(s) + 1)
-	{
-		str[i] = s[start];
-		start++;
-		i++;
-	}
-	str[i] = '\0';
-	return (str);
+	while (*tmp && i < len)
+		ret[i++] = *tmp++;
+	ret[i] = 0;
+	return (ret);
 }
